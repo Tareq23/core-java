f4cba45c409acb9192d4893fa61bf2d4c6cdc871
Tareq23 <mdtarequlislam634@gmail.com>
2021-10-22 16:55:27 +0600

    some built in methods of java string

--- String/src/StringBuiltInMethodCheck.java
+++ String/src/StringBuiltInMethodCheck.java
@@ -2,42 +2,36 @@
 public class StringBuiltInMethodCheck {
 	public static void main(String[] args)
 	{
-//		StringBuilder sb = new StringBuilder("tarequl Islam");
-		String str = "Tarequl Islam";
-		System.out.println("String length : "+str.length());
+		// catcat method
 		
+		String str = "Hello";
+		str.concat(" world");
+		System.out.println(str); // here string str not be changed because String class being immutable
 		
-		// charAt() method
+		StringBuffer sb = new StringBuffer(str);
+//		sb.concat(" world"); // concat method doesn't exists in StringBuffer class
 		
-		// Get a single charater at position 1 as zero base indexing
+		// string will be changed because StringBuffer class is mutable
+		sb.append(" world"); //instead of concat method StringBuffer used append method for string concatenation
+		System.out.println(sb);
+		sb.append(10.4);  // integer value 10 concatenate as string value same as double, char , float
+		System.out.println(sb);
 		
-		System.out.println(str.charAt(1));
-		for(int i=0; i<str.length(); i++)
+		for(int i=0; i<sb.length(); i++)
 		{
-			System.out.printf("%c  ", str.charAt(i));
+			System.out.printf("%c ", sb.charAt(i));
 		}
 		System.out.println();
+		sb.insert(0, 'T');
+		System.out.println(sb);
 		
+		char x = 'A' + 1 ;
+		System.out.println(x);
 		
-		
-		// To Compare with other string
-		
-		String str2 = "tarequl islam";
-		String str3 = "Tarequl Islam";
-		
-		System.out.println("Compare string str with str2 : "+str.compareTo(str2));
-		System.out.println("Compare string str with str3 : "+str.compareTo(str3)); // when both strings are same then return 0
-		String T = "T";
-		String U = "U";
-		String Z = "Z";
-		System.out.println("Compare string U with T : "+U.compareTo(T)); // here U greater than T for one time so return 1;
-		System.out.println("Compare string T with U : "+T.compareTo(U)); // here T less than U for one time so return -1;
-		System.out.println("compate string T with Z : "+T.compareTo(Z)); // Here T less than Z for six time so return -6;
-		System.out.println("compate string Z with T : "+Z.compareTo(T)); // Here T greater than Z for six time so return 6;
-		
-		
-		
-		
-		
+		for(int i=0; i<sb.length(); i++)
+		{
+			sb.replace(i, i+1, "x");
+		}
+		System.out.println(sb);
 	}
 }
